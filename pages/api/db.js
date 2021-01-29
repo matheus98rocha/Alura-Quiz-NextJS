3f2b0bf894a46cc6848c475d0a266970c817ba40
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-01-29 13:50:29 -0300

    update api/db

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,8 +1,14 @@
 import db from '../../db.json'
 
-export function dbHandler (request, res) {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  return res.json(db)
+export default function dbHandler (request, response) {
+  if (request.method === 'OPTIONS') {
+    response.status(200).end()
+    return
+  }
+
+  response.setHeader('Access-Control-Allow-Credentials', true)
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+
+  response.json(db)
 }
